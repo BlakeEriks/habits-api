e4ebe8144a25ea4d73284418fac44cda2d2dc508
Blake Eriks <blakeeriks.dev@gmail.com>
2024-05-07 20:44:28 -0400

    commit

---
+++ prisma/migrations/20240504122503_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `frequency` on the `Habit` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Habit" DROP COLUMN "frequency";
