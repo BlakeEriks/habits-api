2c41a3b171178c0ee795f8a625779e3527987c25
Blake Eriks <blakeeriks.dev@gmail.com>
2024-06-10 12:03:32 +0200

    Big cleanup

--- src/habitBot.ts
+++ src/habitBot.ts
@@ -1,17 +1,27 @@
 import { config } from 'dotenv'
-import { Scenes, Telegraf, session } from 'telegraf'
+import { Scenes, session, Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
-import listHabits from './commands/listHabits'
-import logHabitScene from './commands/logHabit'
-import newHabitScene from './commands/newHabit'
-import removeHabitScene from './commands/removeHabit'
-import timezoneScene from './commands/setTimezone'
-import attachHabits from './middlewares/applyHabits'
+import { HABIT_COMMANDS, HABIT_SCENES } from './commands/habits'
+import { REMINDER_COMMANDS, REMINDER_SCENES } from './commands/reminders'
+import { TIMEZONE_COMMANDS, TIMEZONE_SCENES } from './commands/timezone'
+import attachHabits from './middlewares/attachHabits'
 import attachUser from './middlewares/attachUser'
 import saveMessage from './middlewares/saveMessage'
 import { HabitContext } from './types'
 
 config()
+
+const commandGroups = [
+  { name: 'Habits', commands: HABIT_COMMANDS },
+  { name: 'Reminders', commands: REMINDER_COMMANDS },
+  { name: 'Timezone', commands: TIMEZONE_COMMANDS },
+]
+
+const availableCommands = commandGroups.map(group => {
+  return `${group.name}:\n${group.commands
+    .map(({ name, description }) => `  /${name} - ${description}`)
+    .join('\n')}`
+})
 
 const DEFAULT_MESSAGE = `
 🤖 Beep Boop! 
@@ -21,23 +31,20 @@ I can help you stay accountable to your habits.
 
 Available commands:
 
-- /new -  Create a new habit to track
-- /list - List the habits you are tracking
-- /remove - Remove a habit you are tracking
-- /log - Log your habit data for today
-- /tz - Set your timezone
-- /remind - Set a reminder for your habits
+${availableCommands.join('\n\n')}
 `
 
-if (!process.env.BOT_TOKEN) throw new Error('BOT_TOKEN is required')
+const BOT_TOKEN =
+  process.env.ENV === 'dev' ? process.env.DEV_BOT_TOKEN : process.env.HABIT_BOT_TOKEN
 
-const habitBot = new Telegraf<HabitContext>(process.env.BOT_TOKEN)
+if (!BOT_TOKEN) throw new Error('BOT_TOKEN is required')
+
+const habitBot = new Telegraf<HabitContext>(BOT_TOKEN)
 
 const stage = new Scenes.Stage<HabitContext>([
-  newHabitScene,
-  removeHabitScene,
-  logHabitScene,
-  timezoneScene,
+  ...HABIT_SCENES,
+  ...REMINDER_SCENES,
+  ...TIMEZONE_SCENES,
 ])
 
 habitBot.use(Telegraf.log())
@@ -47,12 +54,11 @@ habitBot.use(attachHabits)
 habitBot.use(saveMessage)
 habitBot.use(stage.middleware())
 
-habitBot.command('new', ctx => ctx.scene.enter('newHabit'))
-habitBot.command('remove', ctx => ctx.scene.enter('removeHabit'))
-habitBot.command('log', ctx => ctx.scene.enter('logHabit'))
-habitBot.command('tz', ctx => ctx.scene.enter('timezone'))
-habitBot.command('remind', ctx => ctx.reply('Not implemented yet.'))
-habitBot.command('list', listHabits)
-habitBot.on(message('text'), async ctx => ctx.reply(DEFAULT_MESSAGE))
+const allCommands = commandGroups.flatMap(({ commands }) => commands)
+for (const { name, action } of allCommands) {
+  habitBot.command(name, action)
+}
 
+// Default
+habitBot.on(message('text'), async ctx => ctx.reply(DEFAULT_MESSAGE))
 export default habitBot
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,15 +1,26 @@
-import { Habit, User } from '@prisma/client'
+import { Habit, Prisma, User } from '@prisma/client'
 import { Context, Scenes } from 'telegraf'
+
+type HabitWithReminders = Prisma.HabitGetPayload<{
+  include: { reminders: true }
+}>
 
 interface HabitSession extends Scenes.SceneSession {
   expecting: string
   habit: Partial<Habit>
   currentHabit: number
+  action: string
 }
 
 interface HabitContext extends Context {
   user: User
-  habits: Habit[]
+  habits: HabitWithReminders[]
   session: HabitSession
   scene: Scenes.SceneContextScene<HabitContext>
+}
+
+interface Command {
+  name: string
+  description: string
+  action: (ctx: HabitContext) => void
 }
