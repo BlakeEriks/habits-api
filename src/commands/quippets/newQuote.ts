2740d3cd873a732a579925f59e9bbf16f387746a
Blake Eriks <blakeeriks.dev@gmail.com>
2024-09-14 10:50:44 -0400

    Support quote creation from telegram

--- src/commands/quippets/newQuote.ts
+++ src/commands/quippets/newQuote.ts
@@ -1,21 +1,27 @@
 import { saveQuote } from '@/prisma-quippet/src/quote.db'
+import { getUserByName } from '@/prisma-quippet/src/user.db'
 import { QuippetContext } from '@/types'
 import { Markup, Scenes } from 'telegraf'
 import { message } from 'telegraf/filters'
 import { replyAndLeave } from '../utils'
 
 export const NEW_QUOTE_SCENE = 'NEW_QUOTE_SCENE'
+const MY_NAME = 'Blake'
+
 enum NEW_QUOTE_FIELDS {
   CONTENT = 'content',
   QUOTEE = 'quotee',
 }
+
 export const newQuoteScene = new Scenes.BaseScene<QuippetContext>(NEW_QUOTE_SCENE)
 newQuoteScene.enter(ctx => {
   ctx.session.expecting = NEW_QUOTE_FIELDS.CONTENT
   ctx.session.quote = {}
   return ctx.reply(`What is the quote?\n\nOr go /back`)
 })
-newQuoteScene.command('back', replyAndLeave('Cancelled habit creation.'))
+
+newQuoteScene.command('back', replyAndLeave('Cancelled quote creation.'))
+
 newQuoteScene.on(message('text'), async ctx => {
   switch (ctx.session.expecting) {
     case NEW_QUOTE_FIELDS.CONTENT:
@@ -25,7 +31,7 @@ newQuoteScene.on(message('text'), async ctx => {
     case NEW_QUOTE_FIELDS.QUOTEE:
       const quotee = ctx.message.text
       const content = ctx.session.quote.content
-      const userId = ctx.user.id
+      const userId = (await getUserByName(MY_NAME))!.id
 
       if (!content) {
         return replyAndLeave('ERROR - Content is missing')(ctx)
